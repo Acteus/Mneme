27a39bfa0b2137cd2df2aa904f5d06eba03307aa
Garren <garrendullas@gmail.com>
2025-12-27 22:33:06 +0800

    Initial Commit

---
+++ MnemeTests/MnemeTests.swift
@@ -0,0 +1,17 @@
+//
+//  MnemeTests.swift
+//  MnemeTests
+//
+//  Created by Garren Dullas on 12/27/25.
+//
+
+import Testing
+@testable import Mneme
+
+struct MnemeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
